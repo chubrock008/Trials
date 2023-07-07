936dabe4ad36924fc29c820c341cf82a1e333592
chubrock <forchuby@gmail.com>
2023-07-07 14:29:09 +0200

    week6 day6 task10

---
+++ week5/reverse-string.ts
@@ -0,0 +1,20 @@
+"use strict";
+
+// Create a function "reverseString" which takes a string input and returns it's reverse
+// Use it on the reversed string below to check it!
+
+// add your code here
+
+let toBeReversed = `.eslaf eb t'ndluow ecnetnes siht ,dehctiws erew eslaf dna eurt fo sgninaem eht fI`;
+
+function reverseString(toBeReversed :string) {
+    let arrayOfStringToReverse = toBeReversed.split("");
+    let arrayOfReverse = arrayOfStringToReverse.reverse();
+    
+    let reverse = arrayOfReverse.join("");
+    console.log(reverse);    
+}
+
+reverseString(toBeReversed)
+
+export { reverseString };
